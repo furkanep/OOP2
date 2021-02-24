311f44857513b1693df7bd349cf4a7066d6e5c88
ensarfurkanpasa@gmail.com <ensarfurkanpasa@gmail.com>
2021-02-24 13:30:58 +0300

    Add project files.

---
+++ OOP3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaselLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            List<ILoggerService> loggers = new List<ILoggerService> { new DataBaseLoggerService(), new FileLoggerService() };
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, new List<ILoggerService> {new DataBaseLoggerService(), new FileLoggerService() });
+            
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager, tasıtKrediManager };
+
+            
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
